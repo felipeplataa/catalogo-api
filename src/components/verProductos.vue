307fc7688330774f65e896775bb54cc6c72d0d14
Felipe Plata <andres.felipe.plata.a@gmail.com>
2020-12-18 10:42:07 -0500

    Ultimo Comit

--- src/components/verProductos.vue
+++ src/components/verProductos.vue
@@ -64,7 +64,7 @@ export default {
     },
     beforeCreate: function(){
         axios
-        .get("https://g3m4-gr10-catalogo-api.herokuapp.com/producto/")
+        .get("https://g3m4-grupo10.herokuapp.com/producto/")
         .then(respuesta => {
             this.database_producto = respuesta.data;
         })
