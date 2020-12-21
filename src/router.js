ee44b7dc7447da4b121d718370b8b0cb3a8ac2b8
felipeplataa <71336009+felipeplataa@users.noreply.github.com>
2020-12-20 21:22:46 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import VueRouter from 'vue-router'
 import AgregarProducto from "./components/IngresarProducto";
 import SacarProducto from "./components/SacarProducto";
 import VerProductos from "./components/verProductos";
+import NuevoProducto from "./components/CrearProducto";
+
 
 
 
@@ -24,8 +26,11 @@ const router= new VueRouter({
                 path:'/ver',
                 component: VerProductos
              },
-             
-
+             {
+                name:'crear',
+                path:'/crear',
+                component: NuevoProducto
+             }
 
         ]
 }
